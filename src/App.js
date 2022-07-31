f84b2fd50263096ad1e92a0e6996f4bdb5a7095f
Kous <kous.takken@gmail.com>
2022-07-31 22:10:20 +0500

    add styles

--- src/App.js
+++ src/App.js
@@ -3,31 +3,54 @@ import axios from "axios";
 
 function App() {
 
-  const [location, setLocation] = useState('Moscow')
+  const [location, setLocation] = useState('')
   const [data, setData] = useState({})
 
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=e4b497eff068841183786921cf2e0901&units=metric`
 
-  const searchLocation = (event) => {
-    if (event.key === 'Enter' || event.key === "click") {
+  const fetchData = () => {
+    try {
       axios.get(url).then((response) => {
         setData(response.data)
         console.log(response.data)
       })
-      setLocation(location)
+    } catch (error) {
+      console.log(error)
     }
   }
 
+  const {name, weather, main} = data
+
   return (
-    <div className="App">
-      <div className="search-container">
-        <input
-          value={location}
-          onChange={event => setLocation(event.target.value)}
-          onKeyPress={searchLocation}
-          placeholder='Enter a city name'
-          type="text" />
-          <button className="search-btns" onClick={searchLocation}>search</button>
+    <div className="wrapper">
+      <div className="container">
+        <div className="search-container">
+          <input
+            value={location}
+            onChange={event => setLocation(event.target.value)}
+            placeholder='Enter a city name'
+            type="text" />
+            <button className="search-btn" onClick={fetchData}>search</button>
+        </div>
+        {name && 
+        <div className="result">
+          <h2>{name}</h2>
+          <h4 className="weather">{weather ? data.weather[0].main : null}</h4>
+          <h4 className="desc">{weather ? data.weather[0].description : null}</h4>
+          <img src={weather ? `https://openweathermap.org/img/w/${data.weather[0].icon}.png` : null} />
+          <h1>{main ? main.temp: null} &#176;</h1>
+          <div className="temp-container">
+            <div>
+              <h4 className="title">min</h4>
+              <h4 className="temp">{main ? data.main.temp_min : null}&#176;</h4>
+            </div>
+            <div>
+            <h4 className="title">max</h4>
+            <h4 className="temp">{main ? data.main.temp_max : null}&#176;</h4>
+            </div>
+          </div>
+        </div>
+        }
       </div>
     </div>
   );
